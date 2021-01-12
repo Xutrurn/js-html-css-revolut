4109ae120adda838c8c79f3e47e1b3cb474449be
Xutrurn <kanjoskillz@gmail.com>
2021-01-12 17:00:40 +0100

    aggiunto click al menu js

--- js/script.js
+++ js/script.js
@@ -4,6 +4,8 @@ $(document).ready(function() {
 
   $('.withDropdown').mouseenter(mostraDropdown);
   $('.withDropdown').mouseleave(nascondiDropdown);
+  $(document).on('click', '.withDropdown', togglaDropdownConClick);
+  $(document).on('click', '.withDropdown.lang', invertiFreccettaConClick);
 
 });
 
@@ -22,6 +24,20 @@ function nascondiDropdown() {
   if ($(this).find('i').hasClass('fa-angle-up')) {
     $('i').removeClass('fa-angle-up');
     $('i').addClass('fa-angle-down');
+  }
+}
+
+function togglaDropdownConClick() {
+  $(this).children('.dropdown').toggleClass('displayFlex');
+}
+
+function invertiFreccettaConClick() {
+  if ($(this).find('i').hasClass('fa-angle-up')) {
+    $('i').removeClass('fa-angle-up');
+    $('i').addClass('fa-angle-down');
+  } else {
+    $('i').removeClass('fa-angle-down');
+    $('i').addClass('fa-angle-up');
   }
 }
 
