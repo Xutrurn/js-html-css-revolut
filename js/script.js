0cf841d636e56e5b557c0946be2173f5db357712
Xutrurn <kanjoskillz@gmail.com>
2021-01-12 16:45:37 +0100

    aggiunto menu dropdown js

--- js/script.js
+++ js/script.js
@@ -2,10 +2,28 @@
 
 $(document).ready(function() {
 
-
+  $('.withDropdown').mouseenter(mostraDropdown);
+  $('.withDropdown').mouseleave(nascondiDropdown);
 
 });
 
+
+
+function mostraDropdown() {
+  $(this).children('.dropdown').addClass('displayFlex');
+  if ($(this).find('i').hasClass('fa-angle-down')) {
+    $('i').removeClass('fa-angle-down');
+    $('i').addClass('fa-angle-up');
+  }
+}
+
+function nascondiDropdown() {
+  $(this).children('.dropdown').removeClass('displayFlex');
+  if ($(this).find('i').hasClass('fa-angle-up')) {
+    $('i').removeClass('fa-angle-up');
+    $('i').addClass('fa-angle-down');
+  }
+}
 
 
 
